0edc56660c785826ddcd10bd059e8ef5fbd9ece8
bhoon-coding <bhooncoding@gmail.com>
2024-07-24 14:32:55 +0900

    Initial Commit

---
+++ StateObserveObjectTest/StateObserveObjectTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateObserveObjectTestApp.swift
+//  StateObserveObjectTest
+//
+//  Created by BH on 2024/07/24.
+//
+
+import SwiftUI
+
+@main
+struct StateObserveObjectTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
